306781693e276c584ce85a8b4523a41ccde3cb11
thespite <the.spite@gmail.com>
2021-04-08 00:02:26 +0100

    cleanup

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const OAC = OfflineAudioContext;
+const OAC = window.OfflineAudioContext || window.webkitOfflineAudioContext;
 const context = new OAC(2, 44100 * 40, 44100);
 
 const canvas = document.createElement("canvas");
@@ -10,11 +10,7 @@ const ctx = canvas.getContext("2d");
 document.body.append(canvas);
 
 const BUCKET_SIZE = Math.round(1 + Math.random() * 10);
-// const TRACK =
-//   "https://cloudflare-ipfs.com/ipfs/QmWKdCra22zqWv8if8JUhbacQEYVVWqF4Avq2p2ESRNf9Q/";
-// const TRACK = "poker-face.mp3";
-const TRACK = "offspring.ogg";
-// const TRACK = `https://cloudflare-ipfs.com/ipfs/QmXEMtojSQnPb2BKEJEKqUPtTbw25BkfBtgmzDNMwUUmqc`;
+const TRACK = "track.mp3";
 
 function getData() {
   const request = new XMLHttpRequest();
@@ -23,29 +19,19 @@ function getData() {
 
   request.responseType = "arraybuffer";
 
-  request.onload = function () {
+  request.onload = () => {
     ctx.fillStyle = "white";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     var audioData = request.response;
-    context.decodeAudioData(audioData, function (buffer) {
-      ctx.strokeStyle = "black";
-      ctx.lineWidth = window.devicePixelRatio * 2.5;
-      ctx.beginPath();
+    context.decodeAudioData(audioData, (buffer) => {
       const data = buffer.getChannelData(0);
-      const step = canvas.width / buffer.duration;
-      for (let i = 0; i < canvas.width; i += BUCKET_SIZE) {
-        const from = ~~((i * data.length) / canvas.width);
-        const to = ~~(((i + 1) * data.length) / canvas.width);
-        let sum = 0;
-        for (let j = from; j < to; j++) {
-          sum += data[j];
-        }
-        const v = (sum * 100000) / (to - from);
-        const h = v;
-        ctx.moveTo(i, 0.5 * canvas.height - h);
-        ctx.lineTo(i, 0.5 * canvas.height + h);
+      for (let i = 0; i < data.length; i++) {
+        const ts = (i * buffer.duration) / data.length;
+        const ptr = ~~((ts * SIZE) / duration);
+        buckets[ptr] += Math.abs(data[i]);
+        samples[ptr]++;
       }
-      ctx.stroke();
+      draw();
     });
   };
   request.send();
@@ -67,10 +53,10 @@ a.src = TRACK;
 let duration;
 a.addEventListener("loadedmetadata", (e) => {
   duration = e.target.duration;
-  console.log(duration);
   e.target.pause();
   delete e.target;
-  fetchData();
+  //fetchData();
+  getData();
 });
 
 let readerDone = false;
@@ -81,7 +67,6 @@ let acDuration = 0;
 function read(reader) {
   reader.read().then(async ({ done, value }) => {
     if (value && value.buffer && value.byteLength > 0) {
-      console.log(value);
       total++;
       try {
         decoded++;
@@ -94,18 +79,10 @@ function read(reader) {
           samples[ptr]++;
         }
         acDuration += buffer.duration;
-        console.log(
-          "decoded",
-          readerDone,
-          decoded,
-          total,
-          acDuration,
-          duration
-        );
         draw();
       } catch (e) {
         decoded++;
-        console.log("error", e, readerDone, decoded, total);
+        // console.log("error", e, readerDone, decoded, total);
       }
     }
     if (!done) {
@@ -114,7 +91,6 @@ function read(reader) {
       readerDone = true;
       console.log("done");
     }
-    // }
   });
 }
 
